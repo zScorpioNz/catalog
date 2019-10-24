abbf282561410963428815aa47f8710ff8d028b6
Cleitontorresweb <cleitonsa1984@gmail.com>
2019-10-24 14:53:30 -0300

    Ajuste link css login

--- src/login.php
+++ src/login.php
@@ -14,7 +14,7 @@
 
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
 
-    <link rel="stylesheet" href="src/assets/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="src/assets/bootstrap.min.css">
     
     <link rel="stylesheet" href="src/assets/fontawesome_5.11/css/all.css">
     <link rel="stylesheet" href="src/custom/css/login.css">
